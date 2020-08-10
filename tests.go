515c32d7697f9fb3dcaeed6e423aa6f98e1991a8
Eatae <al-loco@mail.ru>
2020-08-10 22:49:55 +0300

    tasks_1

--- tests.go
+++ tests.go
@@ -1,7 +1,18 @@
 package main
 
-//
-
 func main() {
+	/* typed const */
+	const constA int = 22
+
+	/* use typed const */
+	var tempA8 int8 = constA //ERROR
+
+	/* untyped const */
+	const constB = 34
+
+	/* use untyped const */
+	var tempB8 int8 = constB   // приведется к типу
+	var tempB16 int16 = constB // приведется к типу
+	var tempB32 int32 = constB // приведется к типу
 
 }
